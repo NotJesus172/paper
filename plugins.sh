9b1e0bc008558b3113f897170b10f8114247590e
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2025-05-10 20:33:52 -0400

    Create plugins.sh

---
+++ plugins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#proxy portals
+#wget
+
+#luck perms
+wget https://cdn.modrinth.com/data/Vebnzrzj/versions/cfNN7sys/LuckPerms-Bukkit-5.4.145.jar
+
+#simple voice chat
+wget https://cdn.modrinth.com/data/9eGKb6K1/versions/KuMfGuTv/voicechat-bukkit-2.5.26.jar
+
+#multiverse core
+wget https://cdn.modrinth.com/data/3wmN97b8/versions/eXnRWJUj/multiverse-core-4.3.16.jar
+
+#multiverse portals
+wget https://cdn.modrinth.com/data/8VMk6P0I/versions/EPEE9mXU/multiverse-portals-4.2.3.jar
+
